230c3e3334abe3c5dc11e4ed3ca1f7fbca2532b0
ju315 <pbju92@gmail.com>
2025-03-28 15:48:18 +0900

    sample2Front:: sample1Front 코드 반영

--- sample2Front/src/index.tsx
+++ sample2Front/src/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import App from './App';
+import { CookiesProvider } from 'react-cookie';
+
+const rootEl = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootEl);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
