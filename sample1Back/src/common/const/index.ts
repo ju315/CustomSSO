809a9e7139d3bfb920f108b3900e64f4e062a6ec
ju315 <pbju92@gmail.com>
2025-04-07 11:59:47 +0900

    sampleBack:: cors 설정 주소 상수화

--- sample1Back/src/common/const/index.ts
+++ sample1Back/src/common/const/index.ts
@@ -1,2 +1,5 @@
 export const AUTH_BACK_PORT = 8081;
 export const AUTH_BACK = `http://192.168.62.13:${AUTH_BACK_PORT}`;
+
+export const SAMPLE_FRONT_PORT = 8000;
+export const SAMPLE_FRONT = `http://192.168.62.13:${SAMPLE_FRONT_PORT}`;
--- sample1Back/src/main.ts
+++ sample1Back/src/main.ts
@@ -3,12 +3,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { VersioningType } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { SAMPLE_FRONT } from './common/const';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: 'http://192.168.62.13:8000',
+    origin: SAMPLE_FRONT,
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
