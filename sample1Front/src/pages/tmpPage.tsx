096ae6b5ade96d583bba5edeb129495f7f52e339
ju315 <pbju92@gmail.com>
2025-04-22 15:53:07 +0900

    sampleFront:: 임시 페이지 수정

--- sample1Front/src/pages/tmpPage.tsx
+++ sample1Front/src/pages/tmpPage.tsx
@@ -1,95 +1,48 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode';
 
-import { setCookie } from '../common/util';
-import ViewTokenData from '../components/viewToken';
-import { TokenData } from '../common/type';
 import { sampleApi } from '../common/axios';
+import ShowMyData from '../components/showMyData';
 
 const TmpPage = () => {
   const [query] = useSearchParams();
   const navigate = useNavigate();
 
-  const [tokenData, setTokenData] = useState({
-    accessToken: '',
-    refreshToken: '',
-  });
+  const [me, setMe] = useState();
 
   useEffect(() => {
-    const at = query.get('at');
-    const rt = query.get('rt');
+    const user = query.get('u');
 
-    if (at && rt) {
-      setCookie(
-        'token',
-        JSON.stringify({
-          accessToken: at,
-          refreshToken: rt,
-        }),
-      );
-
-      setTokenData({
-        accessToken: at,
-        refreshToken: rt,
-      });
-
-      const sid = (jwtDecode(at) as TokenData).uuid;
-
-      if (sid) {
-        setCookie('SSO_PROJECT.sid', sid);
-      }
-
-      saveSignInData(at, rt);
+    if (!user) {
+      goHome();
     }
-  }, []);
+
+    const userData = JSON.parse(user as string);
+    setMe(userData);
+
+    saveSignInData(userData);
+  }, [query]);
 
   const goHome = () => {
-    navigate('/', { replace: true });
+    navigate('/my', { replace: true });
   };
 
-  const saveSignInData = async (at: string, rt: string) => {
+  const saveSignInData = async (userData: any) => {
     try {
-      const res = await sampleApi.post('/api/v2/user/sign-in', {
-        accessToken: at,
-        refreshToken: rt,
-        sessionId: (jwtDecode(at) as TokenData).uuid,
-      });
+      const res = await sampleApi.post('/v3/user/sign-in', { ...userData });
 
       console.log('Save Sign-in data in server result:: ', res.data);
       goHome();
     } catch (err) {
       console.error('Save Sign-in data get error.', err);
-      goHome();
+      // goHome();
     }
   };
 
   return (
     <>
       <h1>Temporary Page</h1>
-      {!!tokenData.accessToken && (
-        <>
-          <ViewTokenData tokenData={tokenData} setTokenData={setTokenData}>
-            <button
-              style={{
-                width: '100%',
-                height: '50px',
-                backgroundColor: '#005fff',
-                color: 'white',
-                cursor: 'pointer',
-                borderRadius: '7px',
-                borderColor: 'white',
-              }}
-              onClick={(e) => {
-                e.preventDefault();
-                goHome();
-              }}
-            >
-              go home
-            </button>
-          </ViewTokenData>
-        </>
-      )}
+      <ShowMyData me={me} />
     </>
   );
 };
